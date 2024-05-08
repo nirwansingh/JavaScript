40859d3d03495cdcb25042903b5b41c0e6059152
Nirwan Singh <71814165+nirwansingh@users.noreply.github.com>
2024-05-08 11:08:47 +0000

    more

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,42 @@
+let score = "22"
+let score2 = "33abc"
+//const {score} = req.body
+//console.log(typeof score)
+let valueInNumber2 = Number(score2)
+let valueInNumber = Number(score) //type casting of string to number
+
+/*
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+console.log(typeof valueInNumber2)
+console.log(valueInNumber2)
+*/
+//NaN is a type found during type casting
+//null value return to 0 when type casted
+//bool return 1 or 0
+//String returns NaN // not a number
+// empty returns false
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// ******************OPERATIONS ******************************
+
+let value = 3
+let nedValue = -value
+console.log(nedValue);
+//** double star result in ^ power */
+
+let str1 = "hello"
+let str2 = "Nirwan"
+let str3 = str1 + str2
+
+console.log(+true);
+console.log(+"");
+
+let num1 , num2 , num3
+num1 =num2=num3 = 2+2
+
+let gameCounter =100
+gameCounter++
+console.log(gameCounter);
---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,17 @@
+// console.log("2" >1);
+// console.log("02" >1);
+/**
+typescript provides certain rules only and is similar to javaScrit
+
+Comparion operation (< or >) converts the null value into integer
+first then compare it, that why it can be a little tricky
+*/
+
+console.log(null >0);
+console.log(null ==0);
+console.log(null >= 0);
+
+console.log(undefined ==0);
+
+// === this is strict check it checks value as well as data types
+
