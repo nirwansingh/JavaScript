fc96d3ce45e7eb168bfb91e83066d7fd65fb8877
Nirwan Singh <71814165+nirwansingh@users.noreply.github.com>
2024-05-08 11:16:56 +0000

    dataType summary

---
+++ 01_basic/datatypes-summary.js
@@ -0,0 +1,23 @@
+/*
+diff of data types is considered here based on the basis of
+how they are stored in the memory and how they are accessed 
+from the memory
+
+i.e, primitive also known as reference type
+7 types : string, number , bool , null , undefined, symbol , bigInt
+
+non primitive
+i.e, Arrays , objects
+ */
+
+const bigNumber = 837284789374892748927n
+
+const heros = ["shaktiman", "nagraj", "doga"];
+let myObj ={
+    name:"hitesh",
+    age:22
+}
+
+const myFunction = function(){
+    console.log("Hollow World");
+}
